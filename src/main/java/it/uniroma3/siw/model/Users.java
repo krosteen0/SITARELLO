b3722707b47ef626745b7d718a381774b87b5454
Christian <christian.carioli@gmail.com>
2025-06-24 16:27:28 +0200

    daje
    da

--- src/main/java/it/uniroma3/siw/model/Users.java
+++ src/main/java/it/uniroma3/siw/model/Users.java
@@ -13,8 +13,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-@Entity
-@Table(name = "users") // Explicitly map to 'users' table
+@Entity(name = "Users")
+@Table(name = "users")
 public class Users {
 
     @Id
